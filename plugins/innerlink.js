e9c096d30eb590c7d563fb5458b56428c3fe19e7
Takashi WADA <twada@amy.hi-ho.ne.jp>
2010-10-09 14:15:47 +0900

    add target="_blank"

--- plugins/innerlink.js
+++ plugins/innerlink.js
@@ -3,8 +3,8 @@ function link(link) {
 	var url = link.href;
 
 	$('rep').style.display = "block";
-	$('reps').innerHTML = '<div id="innerlinkUrl"><a href="' + url 
-		+ '">' + url + '</a></div><iframe id="innerlink" src="' + url
+	$('reps').innerHTML = '<div id="innerlinkUrl"><a target="_blank" href="'
+		+ url + '">' + url + '</a></div><iframe id="innerlink" src="' + url
 		+ '" style="width:100%; height: 350px; display:block">'
 	$('rep').style.top = rep_top;
 	user_pick1 = user_pick2 = null;
