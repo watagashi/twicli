fe5549cd4b97468ddcb4470825935fe36d6ab247
Takashi WADA <twada@amy.hi-ho.ne.jp>
2010-10-23 03:24:37 +0900

    write cookie followers per 200.

--- plugins/followers.js
+++ plugins/followers.js
@@ -1,4 +1,11 @@
+var followers_limit = 200;
 var followers_ids_list = (readCookie('followers_ids') || '');
+var followers_idx = 0, followers_ids_slice;
+while(true) {
+	followers_ids_slice = readCookie('followers_ids' + (followers_idx++)||'')
+	if(!followers_ids_slice) break;
+	followers_ids_list = followers_ids_list.concat(followers_ids_slice);
+}
 followers_ids_list = followers_ids_list != '' ? followers_ids_list.split(',') : [];
 var followers_ids = [];
 for (var i = 0; i < followers_ids_list.length; i++)
@@ -25,7 +32,14 @@ function twfcFollwersIDsClear() {
 	followers_ids_list = followers_ids = [];
 	var status = document.getElementById("followers_status");
 	if (status) status.innerHTML = "off";
-	writeCookie('followers_ids', "", 3652);
+	deleteCookie('followers_ids');
+
+	var followers_idx = 0, followers_ids_slice;
+	while(true) {
+		followers_ids_slice = readCookie('followers_ids' + followers_idx||'')
+		if(!followers_ids_slice) break;
+		deleteCookie('followers_ids' + (followers_idx++));
+	}
 }
 function twfcFollwersIDsRenew() {
 	$("loading").style.display = "block";
@@ -36,8 +50,17 @@ function twfcFollwersIDsRenew() {
 function twfcRenew(list) {
 	followers_ids_list = list;
 	followers_ids = [];
-	for (var i = 0; i < list.length; i++)
+	var begin = 0, end = followers_limit;
+	var followers_idx = 0;
+	for (var i = 0; i < list.length; i++) {
 		followers_ids[list[i]] = 1;
+		if(end < i) {
+			writeCookie('followers_ids' + (followers_idx++), list.slice(begin, end).join(","), 3652);
+			begin = end;
+			end += followers_limit;
+		}
+	}
+	writeCookie('followers_ids' + (followers_idx++), list.slice(begin).join(","), 3652);
 	writeCookie('followers_ids', list.join(","), 3652);
 	var status = document.getElementById("followers_status");
 	if (status) status.innerHTML = "on (" + list.length + ")";
