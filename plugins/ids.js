2fccb7f27b5bf196523ed38803e1ca0643da67c8
Takashi WADA <twada@amy.hi-ho.ne.jp>
2012-02-03 00:09:38 +0900

    plugins/ids.js: Change friends and followers API, because deprecated.

---
+++ plugins/ids.js
@@ -0,0 +1,46 @@
+function switchFollowing() {
+	cur_page = 1;
+	fav_mode = 2;
+	$("tw2c").innerHTML = "";
+	xds.load_for_tab(twitterAPI + 'friends/ids.json?screen_name=' + last_user +
+			'&suppress_response_codes=true', twUsersLookup);
+}
+function switchFollower() {
+	cur_page = 1;
+	fav_mode = 3;
+	$("tw2c").innerHTML = "";
+	xds.load_for_tab(twitterAPI + 'followers/ids.json?screen_name=' + last_user +
+			'&suppress_response_codes=true', twUsersLookup);
+}
+
+var twUserIds = [];
+var twUsers_per_users = 100;
+
+function twUsersLookup(tw) {
+
+	if (tw.error) return error(tw.error);
+	twUserIds = tw.ids||tw;
+	xds.load_for_tab(twitterAPI + 'users/lookup.json' +
+			'?user_id=' + twUserIds.splice(0, twUsers_per_users).join() +
+			'&include_entities=true&suppress_response_codes=true', twUsers);
+}
+function twUsers(tw) {
+	if (tw.error) return error(tw.error);
+	var tmp = $("tmp");
+	if (tmp && tmp.parentNode) tmp.parentNode.removeChild(tmp);
+	var tw2 = tw.map(function(x){
+		if (!x.status) x.status = {'text':'', id:0, 'created_at':x.created_at};
+		x.status.user = x;
+		return x.status;
+	});
+	twShowToNode(tw2, $("tw2c"), false, cur_page > 1, false, false, false, false, false, true);
+	if (twUserIds.length) {
+		$("tw2c").appendChild(nextButton('next'));
+		get_next_func = function() {
+			cur_page++;
+			xds.load_for_tab(twitterAPI + 'users/lookup.json' +
+					'?user_id=' + twUserIds.splice(0, twUsers_per_users).join() +
+					'&include_entities=true&suppress_response_codes=true', twUsers);
+		};
+	}
+}
